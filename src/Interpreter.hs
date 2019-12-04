0cbc56c2a46b5539b98684092f572b7913d58669
Adam Spindler <xmeadamx@gmail.com>
2019-12-03 19:24:53 -0500

    Implement non-threadable monad for error and state

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -7,91 +7,44 @@ import Data.Map
 import Control.Monad.Except;
 import LanguageStructure
 
-{-
-Threads
- - need a way to keep track of the number of 'steps' taken while evaluating so
-   you can evaluate for a fixed number of steps before switching to another
-   thread
-   
- - want a monad that can handle the environment, errors, and thread running out
-   of steps
-
-runThread :: Int -> GscThread -> GscThread
--}
-
-
 type GscEnv = Map LValue Expr
-
-data GscThreadState = GscThreadState [Stmt] GscEnv [String] (IO ())
-
--- GscThread (steps remaining, things to execute, environment, events, application output)
-data GscThread = GscTRunning Int GscThreadState | GscTPaused GscThreadState | GscTError String GscThreadState
-
--- GscAppState CurrentThread [Threads: ] (possibly some global state)
+data GscState = GscState [Stmt] GscEnv (IO ())
+data GscProcess = GscPRunning GscState | GscPError String GscState
 data GscAppState = GscAppState Int [GscM ()] (Maybe GscEnv)
 
 data GscMResult a = GscVal a | GscErr
-newtype GscM a = GscM (GscThread -> (a, GscThread)) 
+newtype GscM a = GscM (GscProcess -> (GscMResult a, GscProcess)) 
 
 instance Functor GscM where
-  --fmap f (GscM t) = GscM (\ thread -> let (x, env') = t thread in (f x, env', pred stps))
-  fmap g (GscM f) = GscM (\ t -> let (x, t') = f t in
-                                 (g x, t')) 
-    
+  fmap g (GscM f) = GscM (\ t -> case f t of
+                                   (GscVal x, t') -> (GscVal (g x), t')
+                                   (GscErr, t')   -> (GscErr, t')) 
+
 instance Applicative GscM where
-  pure x = GscM (x, ) 
+  pure x = GscM (GscVal x, )
   
- -- (GscM t0) <*> (GscM t1) = GscM (\ thread -> let (f, thread', stps')   = t0 (thread, stps)
- --                                                 (x, thread'', stps'') = t1 (thread', stps') in
- --                                                 (f x, thread'', pred stps''))
- 
-  (GscM f0) <*> (GscM f1) = GscM (\ t -> case f0 t of
-                                           (f, GscTRunning stps st) -> case f1 (GscTRunning stps st) of
-                                                                         (x, GscTRunning stps' st') -> (f x, GscTRunning stps' st'))
-                                                                        -- (_, GscTPaused st)         -> ((), GscTPaused st)
-                                                                        -- (_, GscTError err st)      -> ((), GcsTError err st)
-                                           -- (_, GscTPaused st)       -> GscTPaused st
-                                           -- (_, GscTError err st)    -> GscTError err st)
+  (GscM f) <*> (GscM g) = GscM (\ t -> case f t of
+                                         (GscErr, t')     -> (GscErr, t')
+                                         (GscVal fun, t') -> case g t' of
+                                                               (GscErr, t'')     -> (GscErr, t'')
+                                                               (GscVal val, t'') -> (GscVal (fun val), t''))
 
 instance Monad GscM where
   return         = pure
   
-  {-
-     If g returns an error we want to return the error from this function
-  -}
-  (GscM f) >>= g = GscM bnd
-    where
-      checkSwapThread x (GscTRunning 0 st)    = (GscErr, GscTPaused st)
-      checkSwapThread x (GscTRunning stps st) = (x, GscTRunning (pred stps) st)
-      checkSwapThread x t                     = (GscErr, t)
-      
-      bnd t@(GscTRunning stps st) = case f t of
-                                      (x, t' @ (GscTRunning stps' st')) -> case g x of
-                                                                             (GscM f') -> f' x
-                                      (_, t')                           -> (GscErr, t')
-      bnd t = (GscErr, t) -- GscErr needs to be the same type as the return type of g's monad, how?
+  (GscM f) >>= g = GscM (\ t -> case f t of
+                                  (GscVal rx, GscPRunning t') -> let GscM f' = g rx
+                                                                   in f' t 
+                                  (_, GscPError s t')   -> (GscErr, GscPError s t'))
 
 
-getState :: GscM GscThread
-getState = GscRunning (\ (t, stps) -> (t, t, stps))
-
-isDone :: GscM Bool
-isDone = GscRunning (\ (t, stps) -> (stps == 0, t, stps))
-
-checkDone :: GscM ()
-checkDone = do done <- isDone
-               when done $
-                 do t <- getState
-                    GscStopped t
-
---gscError :: GscM String -> GscM ()
---gscError merr = do err <- merr
---                   t   <- getState
---                   GscError err t
+getState :: GscM GscProcess
+getState = GscM (\ t -> (GscVal t, t))
 
 gscError :: String -> GscM a
-gscError msg = do t <- getState
-                  GscError msg t
+gscError err = GscM (\ t -> case t of
+                              (GscPRunning st)    -> (GscErr, GscPError err st)
+                              (GscPError err' st) -> (GscErr, GscPError err' st))
 
 data Value = VString String
            | VList [Value]
@@ -130,54 +83,24 @@ evalBinOp Subtract = evalSub
 evalBinOp Multiply = evalMul
 evalBinOp Divide   = evalDiv
 
---evalExpr :: Expr -> GscThread -> Int -> EvalErr (Value, GscThread, Int)
---evalExpr (StringLit v) t stps      = Right (VString v, t, stps)
---evalExpr (BoolLit v)   t stps      = Right (VBool v, t, stps)
---evalExpr (IntLit v)    t stps      = Right (VInt v, t, stps)
---evalExpr (FloatLit v)  t stps      = Right (VDouble v, t, stps)
---evalExpr (Binary op e1 e2) t stps  = do (val1, t1, stps1) <- evalExpr e1 t  stps
---                                        (val2, t2, stps2) <- evalExpr e2 t1 stps1
---                                        v'                <- evalBinOp op val1 val2
---                                        return (v', t, pred stps2)
-
 evalExpr :: Expr -> Either String Value
 evalExpr expr = runGscM (evalExpr2 (return expr))
 
 evalExpr2 :: GscM Expr -> GscM Value
 evalExpr2 mexpr = do expr <- mexpr
                      case expr of
-                       (IntLit i)         -> return (VInt i)
-                       (StringLit s)      -> return (VString s)
-                       (Binary Add e1 e2) -> do v1 <- evalExpr2 (return e1)
-                                                v2 <- evalExpr2 (return e2)
-                                                case (v1, v2) of
-                                                  (VInt i1, VInt i2) -> return (VInt (i1 + i2))
-                                                  _                  -> gscError "Type Error"
+                       (IntLit i)        -> return (VInt i)
+                       (FloatLit n)      -> return (VDouble n)
+                       (StringLit s)     -> return (VString s)
+                       (Binary op e1 e2) -> do v1 <- evalExpr2 (return e1)
+                                               v2 <- evalExpr2 (return e2)
+                                               evalBinOp op v1 v2 
 
-startThreadState :: GscThread
-startThreadState = GscThread 50 [] empty [] (return ())
+startThreadState :: GscProcess
+startThreadState = GscPRunning (GscState [] empty (return ()))
 
--- runGscM :: GscM a -> Either String a
--- runGscM (GscRunning f) = case f (startThreadState, 50) of
---                               (res, _, _) -> Right res
--- runGscM (GscError e t) = Left e
--- runGscM (GscStopped t) = Left "Stopped"
+runGscM :: GscM a -> Either String a
+runGscM (GscM f) = case f startThreadState of
+                     (GscErr, GscPError err st) -> Left err
+                     (GscVal v, _)              -> Right v
 
-
-{-
-Problem: need to get the current state into this eval, you need the values of the
-         variables that could be used in the expression you're evaluating
-
-How does Haskell know when I say "stmt <- mstmt" that I want to get the statement out
-of the tuple in mstmt?
-
-evalStmt :: GscM Stmt -> GscM ()
-evalStmt mstmt = do stmt <- mstmt
-                    case stmt of
-                      as@(Assign _ _) -> evalAssignStmt stmt
-
-evalAssignStmt :: Stmt -> GscM ()
-evalAssignStmt (Assign lv expr) = do stmt <-
-                                     let v = evalExpr expr in
-                                     store lv v
--}
